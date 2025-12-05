3a6071eaf21c4853b78e495eb08ad9b3c6bac715
Miguel Angel Zambrano <angel245687@gmail.com>
2025-12-04 21:58:04 -0300

    maquetado footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,10 +1,25 @@
-
-
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import logoUrl from '../../assets/icon/logo.png'
 
 const Footer = () => {
   return (
     <div>
-      
+      <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={logoUrl} className='w-50 h-50' rounded />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="#instagram" roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" thumbnail />
+        </Col>
+      </Row>
+    </Container>
     </div>
   )
 }
